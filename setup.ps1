f5a2652b9559bf2c22d0117afddb77377baa3561
Yash Suthar <yashsuthar983@gmail.com>
2025-12-13 11:36:48 +0530

    Added Global setup command to insatll all dependencies in frontend and backend
    Signed-off-by: Yash Suthar <yashsuthar983@gmail.com>

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+Write-Host "Installing Frontend Dependencies..."
+npm install --prefix Alumni-Conncet-Frontend
+
+Write-Host "Installing Backend (open_resume) Dependencies..."
+npm install --prefix Alumni-Connect-backend/open_resume
+
+Write-Host "Setup Complete!"
