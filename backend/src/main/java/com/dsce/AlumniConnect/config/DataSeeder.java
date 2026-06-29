62a4dcebfea625643122ba37c7080ff599cc91fb
Anshul <anshpclg4040@gmail.com>
2026-06-30 01:30:28 +0530

    fixed issue

--- backend/src/main/java/com/dsce/AlumniConnect/config/DataSeeder.java
+++ backend/src/main/java/com/dsce/AlumniConnect/config/DataSeeder.java
@@ -20,13 +20,13 @@ public class DataSeeder implements CommandLineRunner {
         public void run(String... args) throws Exception {
                 if (announcementRepository.count() == 0) {
                         announcementRepository.save(new Announcement("DB: Alumni Meetup 2025",
-                                        "Join us for the annual alumni meetup. (Fetched from Database)", "2 hours ago",
+                                        "Join us for the annual alumni meetup. (Fetched from Database)", "2 hours ago", null,
                                         LocalDateTime.now()));
                         announcementRepository.save(new Announcement("DB: New Mentorship Program",
-                                        "We are launching a new mentorship program. (Fetched from Database)", "1 day ago",
+                                        "We are launching a new mentorship program. (Fetched from Database)", "1 day ago", null,
                                         LocalDateTime.now().minusDays(1)));
                         announcementRepository.save(new Announcement("DB: Campus Recruitment",
-                                        "Top tech companies are visiting. (Fetched from Database)", "2 days ago",
+                                        "Top tech companies are visiting. (Fetched from Database)", "2 days ago", null,
                                         LocalDateTime.now().minusDays(2)));
                 }
         }
